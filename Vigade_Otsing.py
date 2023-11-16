5e17a6393fd40f64c73724dc558588fc461464ea
opilane <opilane@HP-CZC2349HT9>
2023-11-16 16:20:13 +0200

    2

--- Vigade_Otsing.py
+++ Vigade_Otsing.py
@@ -1,12 +1,12 @@
 from ctypes.wintypes import INT
-import math
+import math #не правильно импортирован модуль
 print("Ruudu karakteristikud")
-a = float(input("Sisesta ruudu külje pikkus: "))
+a = float(input("Sisesta ruudu külje pikkus: ")) #не назначен тип
 S = a ** 2
 print("Ruudu pindala", S)
 P = 4*a
 print("Ruudu ümbermõõt", P)
-di = a*math.sqrt(2)
+di = a*math.sqrt(2) #не правильная функция(не дописана)
 print("Ruudu diagonaal", round(di,2))
 print()
 print("Ristküliku karakteristikud")
@@ -17,15 +17,15 @@ print("Ristküliku pindala", S)
 P = 2*(b + c)
 print("Ristküliku ümbermõõt", P)
 di = math.sqrt((b**2)+(c**2))
-print("Ristküliku diagonaal", round(di,2))
+print("Ristküliku diagonaal", round(di,2)) #не дано количество цифр после ","
 print()
-print("Ringi karakteristikud")
-r = float(input('Sisesta ringi raadiusi pikkus => '))
+print("Ringi karakteristikud") #лишняя скобка
+r = float(input('Sisesta ringi raadiusi pikkus => ')) #не назначен тип
 d = 2 * r
 print("Ringi läbimõõt", d)
-S = math.pi * r ** 2
-print("Ringi pindala", round(S,2))
+S = math.pi * r ** 2 #не правильное возведение в степень
+print("Ringi pindala", round(S,2)) #не дано количество цифр после ","
 C = 2* math.pi * r 
-print("Ringjoone pikkus", round(C,2))
+print("Ringjoone pikkus", round(C,2)) #не дано количество цифр после ","
 
 
